ae6e1a48afbea6b221e8f01f1e7b2b2dd857c41d
ilustroc <62394589+ilustroc@users.noreply.github.com>
2026-01-13 10:22:13 -0500

    Move custom mail config to mailing.php
    The impulsego_to and kpinvest_to mail configuration options were moved from mail.php to a new mailing.php file for better separation of custom mail settings.

--- config/mail.php
+++ config/mail.php
@@ -1,14 +1,6 @@
 <?php
 
 return [
-
-    'impulsego_to' => array_values(array_filter(array_map('trim',
-        explode(',', env('IMPULSEGO_MAIL_TO', ''))
-    ))),
-
-    'kpinvest_to'  => array_values(array_filter(array_map('trim',
-        explode(',', env('KPINVEST_MAIL_TO', ''))
-    ))),
     
     /*
     |--------------------------------------------------------------------------
